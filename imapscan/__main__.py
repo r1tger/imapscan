2dc5742d8fd56173c741686927efef4ea58be0ff
ritger <github_r1tger@ritger.nl>
2018-10-28 21:55:04 +0100

    Add Pandas support for statistics

--- imapscan/__main__.py
+++ imapscan/__main__.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pandas as pd
+
 from argparse import ArgumentParser
 from imaplib import IMAP4_SSL
 from email import message_from_string
 from email.policy import SMTPUTF8
 
 from sys import exit
+from re import search
 
 import logging
 log = logging.getLogger(__name__)
@@ -45,6 +48,11 @@ def parse():
                         help='IMAP mailboxes to process')
     parser.add_argument('--find', default='', required=True,
                         help='search criteria to match e-mails against')
+    parser.add_argument('--limit', help='max number of messages to retrieve',
+                        type=int)
+    file_operations = parser.add_mutually_exclusive_group()
+    file_operations.add_argument('--in-file', help='input DataFrame (CSV)')
+    file_operations.add_argument('--out-file', help='output DataFrame (CSV)')
     # Parse options
     return parser.parse_args()
 
@@ -74,6 +82,7 @@ def get_messages(options):
             log.info('Login user: "{u}"'.format(u=options.username))
             imap.login(user=options.username, password=options.password)
             try:
+                # Process each mailbox
                 for mailbox in options.mailbox:
                     # Keep track of retrieved files
                     if mailbox not in retrieved:
@@ -86,7 +95,8 @@ def get_messages(options):
                                           s=options.find))
                     # Remove retrieved messages from list
                     found = data[0].split()
-                    # found = found[1:5]
+                    if options.limit:
+                        found = found[1:options.limit]
                     found = [x for x in found if x not in retrieved[mailbox]]
                     log.info('Processing {n} messages'.format(n=len(found)))
                     # Get message
@@ -103,37 +113,111 @@ def get_messages(options):
                 log.info('Lost connection, reconnecting')
                 continue
             else:
-                log.info('Logout user: "{u}"'.format(u=options.username))
                 # Logout before continuing
+                log.info('Logout user: "{u}"'.format(u=options.username))
                 imap.logout()
     # Success
     return messages
 
 
-def main():
-    """Main entry point
-    :returns: TODO
+def get_rows(messages):
+    """ """
+    rows = []
+    for msg in messages:
+        try:
+            # Create a new row based on the EmailMessage
+            date = pd.to_datetime(msg['Date'])
+            row = {'From': msg['From'], 'To': msg['To'],
+                   'Subject': msg['Subject'],
+                   'Date': date,
+                   'Month': date.to_period('M'),
+                   'X-Spam-Flag': msg['X-Spam-Flag'],
+                   'Message-ID': msg['Message-ID'],
+                   'Has-Attachment': 'Yes' if has_attachment(msg) else 'No'}
+            rows.append(row)
+            # log.debug(msg.keys())
+            # log.info('{d} | {f: <40.40} | {s: <40.40}'.format(
+            #          f=msg['From'], s=msg['Subject'], d=msg['Date']))
+        except TypeError:
+            continue
+    return rows
 
-    Statistics:
-    - List of unique e-mail addresses
-    - Nr. of e-mails per month
-    - Nr. E-mails with attachments
-    """
+
+def get_unique_addresses(df):
+    """ """
+    addresses = []
+    for _, row in df.iterrows():
+        addresses += get_unique_address(row['From'])
+        addresses += get_unique_address(row['To'])
+    return set(addresses)
+
+
+def get_unique_address(row):
+    """ """
+    addresses = []
+    for ea in row.split(', '):
+        m = search(r'<(.+)>', ea)
+        address = m.group(1) if m is not None else ea.strip()
+        if '@' in address:
+            # Quick sanity check
+            addresses.append(address)
+    return addresses
+
+
+def has_attachment(msg):
+    """ Process each multipart of a message and check if an attachment is
+    found """
+    for part in msg.walk():
+        if part.is_attachment():
+            return True
+    return False
+
+
+def format_series(series):
+    """ """
+    values = []
+    for index, value in series.items():
+        values.append('({i}, {v})'.format(i=index, v=value))
+    return ' '.join(values)
+
+
+def main():
+    """ Main entry point """
     options = parse()
     try:
         # Setup logging
         logger(options)
 
-        # Retrieve all messages from the server
-        messages = get_messages(options)
-        log.info('{s:-^80}'.format(s=' E-mail Messages '))
-        for msg in messages:
-            try:
-                # log.debug(msg.keys())
-                log.info('{d} | {f: <40.40} | {s: <40.40}'.format(
-                         f=msg['From'], s=msg['Subject'], d=msg['Date']))
-            except TypeError:
-                continue
+        if options.in_file:
+            # Load CSV provided on command line
+            df = pd.read_csv(options.in_file, encoding='utf-8')
+        else:
+            # Retrieve all messages from the server
+            messages = get_messages(options)
+            log.info('{s:-^80}'.format(s=' E-mail Messages '))
+            # Retrieve all rows for the messages
+            rows = get_rows(messages)
+
+            # Create a new DataFrame from the rows
+            df = pd.DataFrame(rows)
+            if options.out_file:
+                df.to_csv(options.out_file, encoding='utf-8', index=False)
+
+        log.info('{s:-^80}'.format(s=' Statistics '))
+
+        # Number of e-mails
+        log.info('Total number of e-mails: {c}'.format(c=len(df)))
+        # List of unique e-mail addresses
+        addresses = get_unique_addresses(df)
+        log.info('Unique e-mail addresses: {e}'.format(e=len(addresses)))
+        log.debug(addresses)
+
+        log.info('{s:-^80}'.format(s=' Table Data '))
+
+        # Number of e-mails per month
+        log.info(format_series(df.groupby('Month').size()))
+        # Number of e-mails that have an attachment
+        log.info(format_series(df.groupby('Has-Attachment').size()))
 
         # Success
         return(0)
